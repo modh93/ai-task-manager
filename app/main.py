ed035a8727cd164beb824902464befdd1f54138e
Mohamed KEITA <contact@mohamedkeita.pro>
2026-05-25 23:22:49 +0200

    feat: add user model and auto-create tables

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 
+from app.db.database import Base
+from app.db.database import engine
+
+
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
---
+++ app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+
+from app.db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    full_name = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
